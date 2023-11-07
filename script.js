840bae697476cbb1cb546ccea57c13e4eb3c6f72
VANSHKALRA88 <136835917+VANSHKALRA88@users.noreply.github.com>
2023-11-07 21:31:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text" , {
+    strings:["Astrologer","Vedic","Numerologist","Palmist","Tarot Reader","Vastu Specialist"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+})
